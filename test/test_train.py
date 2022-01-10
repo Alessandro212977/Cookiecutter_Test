b904ade4840cd798b7b991f01b5ae490de39864e
Alessandro212977 <s212977@dtu.dk>
2022-01-10 11:24:11 +0100

    fixing again

--- test/test_train.py
+++ test/test_train.py
@@ -2,8 +2,9 @@ import sys
 sys.path.insert(1, "./src/models")
 
 from train_model import train
-import os
+import os, pytest
 
+@pytest.mark.skipif(not os.path.exists("data/processed/train_dataset.pt"), reason='data files not found')
 def test_training():
     train()
     assert os.path.exists("./models/checkpoint.pth")
